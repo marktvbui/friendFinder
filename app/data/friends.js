aeb5d9dd929a740520dd62b85f06249299b75788
Mark Bui <tvyybui@yahoo.com>
2017-08-01 16:53:20 -0700

    started 2 new functions, one to make html more dry, other for bubble sort

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,8 @@
-var friendsArray =
-[
+var friendsArray = [
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
+    name: "Hipster",
+    photo: "https://s-media-cache-ak0.pinimg.com/736x/42/a9/b7/42a9b7d92b940a8754b04f9faaf64865--emoji-wallpaper-hipster-wallpaper.jpg",
+    answers: [
         5,
         1,
         4,
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,9 @@ module.exports = function(app) {
   });
 
   app.post('/api/friends', function (req, res) {
+    console.log(req.body);
     friendData.push(req.body);
+    res.json(friendData);
   });
 
 }
