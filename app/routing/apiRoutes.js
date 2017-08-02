4e4162fdae9b53c997873997c7eea0657c8495a0
Mark Bui <tvyybui@yahoo.com>
2017-08-02 03:22:45 -0700

    finished survey.html, working on final api routes/logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,10 +2,17 @@ var friendData = require('../data/friends.js');
 
 module.exports = function(app) {
 
+  var user = [];
+
   app.get('/api/friends', function (req, res) {
     res.json(friendData);
+    friendData.forEach(function(data) {
+      var name = data.name
+      var answer = data.answers
+      user.push(name, answer);
+      console.log(user);
+    })
   });
-
   app.post('/api/friends', function (req, res) {
     // var answers = req.body['answers[]'];
     // var name = req.body.name;
@@ -13,6 +20,7 @@ module.exports = function(app) {
     console.log(req.body);
     friendData.push(req.body);
     res.json(friendData);
+
   });
 
 }
