9ee1b0834356cc48bd93083408dfd027173f7939
Mark Bui <tvyybui@yahoo.com>
2017-08-02 17:40:53 -0700

    fixed post request, from post to ajax/post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,20 +2,24 @@ var friendData = require('../data/friends.js');
 
 module.exports = function(app) {
 
-  var user = [];
+  // var user = [];
 
   app.get('/api/friends', function (req, res) {
     res.json(friendData);
-    friendData.forEach(function(data) {
-      var name = data.name
-      var answer = data.answers
-      user.push(name, answer);
-      console.log(user);
-      return user;
-    })
+    // friendData.forEach(function(data) {
+    //   var name = data.name
+    //   var answer = data.answers
+    //   user.push(name, answer);
+    //   console.log(user);
+    //   return user;
+    // })
   });
+
   app.post('/api/friends', function (req, res) {
-    var answers = req.body['answers[]'];
+    friendData.push(req.body);
+    // console.log(friendData);
+    // console.log(req.body);
+    var answers = req.body;
     var differenceArray = [];
     for (var j = 0; j < friendData.length; j++) {
       var difference = 0;
@@ -24,7 +28,7 @@ module.exports = function(app) {
       }
       differenceArray.push(difference);
     };
-    console.log(differenceArray);
+
     var index = 0;
     var value = differenceArray[0];
     for (var x = 0; x < differenceArray.length; x++) {
