4cae5c472e0025a745a6122019c8461080c2253a
Mark Bui <tvyybui@yahoo.com>
2017-08-02 17:44:47 -0700

    removed un needed code, added comments to code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,41 +2,36 @@ var friendData = require('../data/friends.js');
 
 module.exports = function(app) {
 
-  // var user = [];
-
   app.get('/api/friends', function (req, res) {
     res.json(friendData);
-    // friendData.forEach(function(data) {
-    //   var name = data.name
-    //   var answer = data.answers
-    //   user.push(name, answer);
-    //   console.log(user);
-    //   return user;
-    // })
   });
 
   app.post('/api/friends', function (req, res) {
     friendData.push(req.body);
-    // console.log(friendData);
-    // console.log(req.body);
     var answers = req.body;
     var differenceArray = [];
+    //looping through each object in the friendsArray
     for (var j = 0; j < friendData.length; j++) {
       var difference = 0;
+      // within each friend, we are getting the difference in answer scores
       for (var i = 0; i < answers.length; i++){
         difference += Math.abs( parseInt(friendData[j].answers[i]) - parseInt(answers[i]) );
       }
+      // pushing the scores into the differenceArray
       differenceArray.push(difference);
     };
 
     var index = 0;
     var value = differenceArray[0];
+    // looping through array finding the lowest score
     for (var x = 0; x < differenceArray.length; x++) {
       if (differenceArray[x] < value) {
         value = differenceArray[x];
+        // assigning index to the lowest score
         index = x;
       }
     }
+    // returning the friend with the lowest difference in score
     res.json(friendData[index]);
   });
 
